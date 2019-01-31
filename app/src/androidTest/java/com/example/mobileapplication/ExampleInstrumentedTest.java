538528363f80855ea2ac028a214f468f91f00ea4
torontotong <torontotong@gmail.com>
2019-01-31 17:25:28 -0500

    Added Espresso testing code for login form.

--- app/src/androidTest/java/com/example/mobileapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mobileapplication/ExampleInstrumentedTest.java
@@ -2,12 +2,24 @@ package com.example.mobileapplication;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,6 +28,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(LoginActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -23,4 +39,15 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.mobileapplication", appContext.getPackageName());
     }
+
+    @Test
+    public void user_login() throws InterruptedException {
+        onView(withId(R.id.userName)).perform(typeText("Hello"));
+        onView(withId(R.id.password)).perform(typeText("World"));
+        onView(withId(R.id.systolic)).perform(typeText("120"));
+        onView(withId(R.id.diastolic)).perform(typeText("75"));
+ //       onView(withId(R.id.sign_in_button)).perform(click()).check(matches(isClickable()));
+    }
+
+
 }
